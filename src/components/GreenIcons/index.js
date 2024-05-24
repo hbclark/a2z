269927ba077f64b3274df1f07d841aaaf5062847
clark he <arcaneade@gmail.com>
2024-05-24 20:41:18 +1000

    before antd

---
+++ src/components/GreenIcons/index.js
@@ -0,0 +1,14 @@
+export default function GreenIcons(){
+
+    return (
+        <div>
+
+
+<a href="https://www.freepik.com/search">Icon by Freepik</a>
+<a href="https://www.freepik.com/search">Icon by Nhor Phai</a>
+        </div>
+    )
+    
+    
+    
+}
